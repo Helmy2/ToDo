05ed2aeddcd577eb067cebf15f77575dfcdc077b
Helmy <mohamed0132525715@gmail.com>
2022-09-01 11:31:40 +0200

    Style: add theme

---
+++ app/src/main/java/com/example/todo/presentation/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.example.todo.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightPrimary = Color(0xFF51A4E1)
+val LightSecondary = Color(0xFFE4E3E9) // F7F7F7
+val LightSecondaryVariant = Color(0xFFFFFFFF)
+val LightBackground = Color(0xFFF3F2F8)
+val LightError = Color(0xFFEA6D7E)
+
+val TwilightPrimary = Color(0xFF319DE7)
+val TwilightSecondary = Color(0xFF383944)
+val TwilightSecondaryVariant = Color(0xFF2A2C3A)
+val TwilightBackground = Color(0xFF202230)
+val TwilightOn = Color(0xFFFDFDFD)
+
+
+val Error = Color(0xFFB00020)
+
+val ListRed = Color(0xFFFF453A)
+val ListOrange = Color(0xFFFF9F0A)
+val ListYellow = Color(0xFFFFD60A)
+val ListGreen = Color(0xFF30D15B)
+val ListBlue = Color(0xFF0A84FF)
+val ListPurple = Color(0xFF5E5CE6)
+val ListBrown = Color(0xFFC9A675)
+
+val CommonGrey = Color(0xFF5B626A)
---
+++ app/src/main/java/com/example/todo/presentation/theme/Theme.kt
@@ -0,0 +1,68 @@
+package com.example.todo.presentation.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+val TwilightColorPalette = lightColorScheme(
+    primary = TwilightPrimary,
+    primaryContainer = TwilightPrimary,
+    onPrimary = TwilightOn,
+    onPrimaryContainer = TwilightOn,
+    secondary = TwilightSecondary,
+    secondaryContainer = TwilightSecondaryVariant,
+    onSecondary = TwilightOn,
+    onSecondaryContainer = TwilightOn,
+    background = TwilightBackground,
+    onBackground = TwilightOn,
+    surface = TwilightSecondaryVariant,
+    onSurface = TwilightOn,
+    error = Error,
+    onError = Color.White
+)
+
+val LightColorPalette = lightColorScheme(
+    primary = LightPrimary,
+    primaryContainer = LightPrimary,
+    onPrimary = Color.White,
+    onPrimaryContainer = Color.White,
+    secondary = LightSecondary,
+    secondaryContainer = LightSecondaryVariant,
+    onSecondary = Color.Black,
+    onSecondaryContainer = Color.Black,
+    background = LightBackground,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black,
+    error = LightError,
+    onError = Color.White
+)
+
+@Composable
+fun ToDoTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        TwilightColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = colors.surface,
+            darkIcons = !darkTheme,
+            isNavigationBarContrastEnforced = false,
+        )
+    }
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
